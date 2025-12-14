2e870f2c407ff2df5204a023f89ce27846b15982
tjdux <tjduxxx@gmail.com>
2025-12-14 17:52:18 +0900

    feature: 로그인, 소셜 로그인

--- onebite-log/src/api/auth.ts
+++ onebite-log/src/api/auth.ts
@@ -1,4 +1,5 @@
 import supabase from "@/lib/supabase";
+import type { Provider } from "@supabase/supabase-js";
 
 export async function singUp({
   email,
@@ -11,6 +12,31 @@ export async function singUp({
     email,
     password,
   });
+
+  if (error) throw error;
+
+  return data;
+}
+
+export async function signInWithPassword({
+  email,
+  password,
+}: {
+  email: string;
+  password: string;
+}) {
+  const { data, error } = await supabase.auth.signInWithPassword({
+    email,
+    password,
+  });
+
+  if (error) throw error;
+
+  return data;
+}
+
+export async function signInWithOAuth(provider: Provider) {
+  const { data, error } = await supabase.auth.signInWithOAuth({ provider });
 
   if (error) throw error;
 
--- onebite-log/src/pages/sign-in-page.tsx
+++ onebite-log/src/pages/sign-in-page.tsx
@@ -1,7 +1,69 @@
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { useSignInWithPassword } from "@/hooks/mutations/use-sign-in-with-password";
+import { useState } from "react";
+import { Link } from "react-router";
+import gitHubLogo from "@/assets/github-mark.svg";
+import { useSignInWithOAuth } from "@/hooks/mutations/use-sign-in-with-oauth";
+
 export default function SignInPage() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const { mutate: signInWithPassword } = useSignInWithPassword();
+  const { mutate: signInWithOAuth } = useSignInWithOAuth();
+
+  const handleSignInWithPasswordClick = () => {
+    if (email.trim() === "") return;
+    if (password.trim() === "") return;
+
+    signInWithPassword({
+      email,
+      password,
+    });
+  };
+
+  const handleSignInWithGithubClick = () => {
+    signInWithOAuth("github");
+  };
+
   return (
-    <>
-      <h1>SignInPage Component</h1>
-    </>
+    <div className="flex flex-col gap-8">
+      <div className="text-xl font-bold">로그인</div>
+      <div className="flex flex-col gap-2">
+        <Input
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          className="py-6"
+          type="email"
+          placeholder="example@abc.com"
+        />
+        <Input
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          className="py-6"
+          type="password"
+          placeholder="password"
+        />
+      </div>
+      <div className="flex flex-col gap-2">
+        <Button className="w-full" onClick={handleSignInWithPasswordClick}>
+          로그인
+        </Button>
+        <Button
+          className="w-full"
+          variant={"outline"}
+          onClick={handleSignInWithGithubClick}
+        >
+          <img src={gitHubLogo} className="h-4 w-4" />
+          Github 계정으로 로그인
+        </Button>
+      </div>
+      <div>
+        <Link className="text-muted-foreground hover:underline" to={"/sign-up"}>
+          계정이 없으시다면? 회원가입
+        </Link>
+      </div>
+    </div>
   );
 }
