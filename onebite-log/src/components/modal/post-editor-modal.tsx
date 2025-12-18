e97ae790e44893ec1c0395f83ba43af391da7d1e
tjdux <tjduxxx@gmail.com>
2025-12-18 18:15:40 +0900

    포스트 추가 기능

---
+++ onebite-log/src/components/modal/post-editor-modal.tsx
@@ -0,0 +1,78 @@
+import { Button } from "@/components/ui/button";
+import { Dialog, DialogContent, DialogTitle } from "@/components/ui/dialog";
+import { useCreatePost } from "@/hooks/mutations/post/use-create-post";
+import { usePostEditorModal } from "@/store/post-editor-modal";
+import { ImageIcon } from "lucide-react";
+import { useEffect, useRef, useState } from "react";
+import { toast } from "sonner";
+
+export default function PostEditorModal() {
+  const { isOpen, close } = usePostEditorModal();
+  const { mutate: createPost, isPending: isCreatePostPending } = useCreatePost({
+    onSuccess: () => {
+      close();
+    },
+    onError: () => {
+      toast.error("포스트 생성에 실패했습니다.", {
+        position: "top-center",
+      });
+    },
+  });
+
+  const [content, setContent] = useState("");
+  const textareaRef = useRef<HTMLTextAreaElement>(null);
+
+  const handleCloseModal = () => {
+    close();
+  };
+
+  const handleCreatePostClick = () => {
+    if (content.trim() === "") return;
+    createPost(content);
+  };
+
+  useEffect(() => {
+    if (textareaRef.current) {
+      textareaRef.current.style.height = "auto";
+      textareaRef.current.style.height =
+        textareaRef.current.scrollHeight + "px";
+    }
+  }, [content]);
+
+  useEffect(() => {
+    if (!isOpen) return;
+    textareaRef.current?.focus();
+    setContent("");
+  }, [isOpen]);
+
+  return (
+    <Dialog open={isOpen} onOpenChange={handleCloseModal}>
+      <DialogContent className="max-h-[90vh]">
+        <DialogTitle>포스트 작성</DialogTitle>
+        <textarea
+          className="max-h-125 min-h-25 focus:outline-none"
+          placeholder="무슨 일이 있었나요?"
+          value={content}
+          onChange={(e) => setContent(e.target.value)}
+          ref={textareaRef}
+          disabled={isCreatePostPending}
+        />
+        <Button
+          variant={"outline"}
+          className="cursor-pointer"
+          disabled={isCreatePostPending}
+        >
+          <ImageIcon />
+          이미지 추가
+        </Button>
+        <Button
+          className="cursor-pointer"
+          onClick={handleCreatePostClick}
+          disabled={isCreatePostPending}
+        >
+          저장
+        </Button>
+      </DialogContent>
+    </Dialog>
+  );
+}
--- onebite-log/src/pages/index-page.tsx
+++ onebite-log/src/pages/index-page.tsx
@@ -1,7 +1,9 @@
+import CreatePostButton from "@/components/post/create-post-button";
+
 export default function IndexPage() {
   return (
-    <>
-      <h1>IndexPage Component</h1>
-    </>
+    <div className="flex flex-col gap-10">
+      <CreatePostButton />
+    </div>
   );
 }
